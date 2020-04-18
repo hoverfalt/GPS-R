35cc97322b1907d9af515e35fddf210fff376727
Olof Hoverfalt <hoverfalt@gmail.com>
2020-04-18 21:45:37 +0300

    Added distance calculation from GPS data. Refactored data cleaning. Combined date and time to datetime.

--- GPS.R
+++ GPS.R
@@ -1,66 +1,131 @@
-### GPS.R
+### GPS.R - Playground for manipulating and plotting GPS data
 
 
+##########################
 ### SET UP ENVIRONMENT ###
+##########################
 
+# Remove all objects from workspace
+rm(list = ls())
+
+# Load required libraries
 library(dplyr)
 library(lubridate)
 library(ggmap)
+library(chron)
+library(geosphere)
 
 # Source required files
 source("GPS-R-API-key.R")
 
-# Set Google API Key, enabled for Maps Static API
+# Set Google API Key (Maps Static API)
 register_google(key = get_google_key())
 has_google_key()
 
 
+###########################
 ### READ AND CLEAN DATA ###
+###########################
 
 # Set csv data column classes to avoid data loss (e.g. non-delimited time parsed as numeric)
 csv_column_classes = c("character", "character", "factor", "character", "factor", "character", "factor", "numeric", "numeric",
                "character", "numeric", "factor", "character")
 
 # Read csv data
-rawdata <- read.csv("Västerby.csv", colClasses = csv_column_classes)
+gpsdata <- read.csv("Västerby.csv", colClasses = csv_column_classes)
 
 # Set variable names
-colnames(rawdata) <- c("gprmc", "utc", "status", "lat1", "lat2", "lon1", "lon2", "speed", "track",
+colnames(gpsdata) <- c("gprmc", "utc", "status", "lat1", "lat2", "lon1", "lon2", "speed", "track",
                        "date", "mag_var1", "mag_var2", "checksum")
 
 # Convert times and dates to correct data types
-rawdata <- rawdata %>%
+gpsdata <- gpsdata %>%
   mutate(date = dmy(date), utc = as.times(paste(substr(utc, 1, 2),':', substr(utc, 3, 4),':',substr(utc, 5, 6), sep = "")))
 
+# Combine date and time into POSIXct (datetime)
+gpsdata$timestamp <- as.POSIXct(paste(gpsdata$date, gpsdata$utc), origin = "1970-01-01")
+
 # Convert position data to numeric
-rawdata <- rawdata %>%
+gpsdata <- gpsdata %>%
   mutate(lat = as.numeric(sp::char2dms(paste(substr(lat1, 1, 2),"d", substr(lat1, 3, nchar(lat1)),"'", lat2, sep = "")))) %>%
   mutate(lon = as.numeric(sp::char2dms(paste(substr(lon1, 1, 3),"d", substr(lon1, 4, nchar(lon1)),"'", lon2, sep = ""))))
 
+# Drop redundant variables
+gpsdata <- gpsdata %>% select(-gprmc, -lat1, -lat2, -lon1, -lon2, -date, -utc)
 
-str(rawdata)
-rawdata
+# Rearrange variables
+gpsdata <- gpsdata[c("timestamp", "status", "lat", "lon", "speed", "track", "mag_var1", "mag_var2", "checksum")]
+
+# Show structure of rawdata
+str(gpsdata)
+head(gpsdata, 10)
 
 
-
+#################################
 ### PLOT POSITION DATA ON MAP ###
+#################################
 
+# Calculate the center location of the data points
+centerLocation <- c(lon = mean(c(min(gpsdata$lon), max(gpsdata$lon))),
+                    lat = mean(c(min(gpsdata$lat), max(gpsdata$lat))))
 
-# Set start location
-myLocation <- c(lon = rawdata$lon[1], lat = rawdata$lat[1])
+# Set up map (TO-DO: the zoom level should be set to fit the data)
+map <- get_googlemap(center = centerLocation,
+                     maptype = "terrain",
+                     zoom = 13)
 
-# Set up map
-myMap <- get_map(myLocation,
-                maptype = "terrain",
-                source = "google",
-                crop = FALSE,
-                zoom = 16)
+# Setup plot - points
+map_plot <- ggmap(map) +
+  geom_point(data = gpsdata, aes(x = lon, y = lat, color = speed), size = 1, lineend = "round") +
+  scale_colour_gradient(low = "green", high = "red")
+
+# Setup plot - path
+map_plot <- ggmap(map) +
+  geom_path(data = gpsdata, aes(x = lon, y = lat, color = speed), size = 1, lineend = "round") +
+  scale_colour_gradient(low = "green", high = "red")
+
 # Plot map
-map_plot <- ggmap(myMap)
 map_plot
 
 # Save map as png image
-ggsave(filename = "map_plot.png", map_plot,
+ggsave(filename = "Plots/map_plot.png", map_plot,
+       width = 300, height = 250, dpi = 300, units = "mm", device='png')
+
+
+###########################
+### MANIPULTE GPS DATA  ###
+###########################
+
+# Function to calculate the distance between two data points
+calculate_distance <- function(lon1, lat1, lon2, lat2){
+  return(distm(c(lon1, lat1), c(lon2, lat2), fun = distHaversine))
+}
+
+# Calculate distance between data ponts n and n-1
+gpsdata <- gpsdata %>%
+  mutate(lat_prev = lag(lat), lon_prev = lag(lon)) %>%
+  mutate(dist = purrr::pmap_dbl(list(lon, lat, lon_prev, lat_prev), calculate_distance)) %>%
+  select(-lat_prev, -lon_prev)
+
+# Set first distance data point from NA to 0
+gpsdata$dist[1] <- 0
+
+# Calculate cumulative distance
+gpsdata <- gpsdata %>% mutate(cum_dist = cumsum(dist))
+
+# Print toal distance
+sum(gpsdata$dist)
+gpsdata$cum_dist[nrow(gpsdata)]
+
+# Plot cumulative distance over time
+p <- ggplot(gpsdata, aes(x = timestamp, y = cum_dist)) +
+  geom_line() +
+  scale_x_datetime() +
+  labs(x = "Time", y = "Distance (m)")
+p
+
+# Save plot as png
+ggsave(filename = "Plots/distance_over_time.png", p,
        width = 300, height = 250, dpi = 300, units = "mm", device='png')
 
 
@@ -71,3 +136,4 @@ ggsave(filename = "map_plot.png", map_plot,
 # https://www.nceas.ucsb.edu/sites/default/files/2020-04/ggmapCheatsheet.pdf
 # https://eriqande.github.io/rep-res-web/lectures/making-maps-with-R.html
 # https://www.jessesadler.com/post/geocoding-with-r/ 
+
